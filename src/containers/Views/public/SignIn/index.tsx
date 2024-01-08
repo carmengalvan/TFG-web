b36d8e4e6fccbe4a557e27f49845eff0dbcf0761
Carmen <carmengalvan@z1.digital>
2024-01-08 09:39:48 +0100

    fix: form style changes

--- src/containers/Views/public/SignIn/index.tsx
+++ src/containers/Views/public/SignIn/index.tsx
@@ -27,55 +27,61 @@ export function SignInView(){
     return (
         <RootLayout>
             <Auth imageSrc='/images/inicio_sesion.png'>
-                <div className="max-w-md mx-auto bg-white p-8 rounded-3xl shadow-md w-full">
-                <p className="text-center mb-1">Inicia sesión</p>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-                        <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="mail@example.com" 
-                                type = 'email'
-                                {...field} />
-                            </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-                        <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem className="mb-4">
-                            <FormLabel>Contraseña</FormLabel>
-                            <FormControl>
-                                <Input 
-                                type = 'password'
-                                placeholder="Enter your password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-                        <div className="text-center">
-                            <Button type="submit">INICIAR SESIÓN</Button>
-                        </div>
-                    </form>
-                    <div className="mx-auto my-4 flex w-full items-center justify-evenly
-                    before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
-                    after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
-                    o
+                <div className="mx-auto bg-gray-200 p-8 rounded-3xl shadow-md w-2/5">
+                    <p className="text-center text-2xl">Inicia sesión</p>
+                    <div className="mx-auto bg-white p-8 rounded-3xl shadow-md w-full">
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                                <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="mail@example.com" 
+                                        type = 'email'
+                                        {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem className="mb-4">
+                                    <FormLabel>Contraseña</FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                        type = 'password'
+                                        placeholder="Enter your password" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <div className="text-right">
+                                <Link className='text-teal-600 text-sm' href='/'>¿Has olvidado tu contraseña?</Link>
+                                </div>
+                                <br></br>
+                                <div className="text-center">
+                                    <Button type="submit">INICIAR SESIÓN</Button>
+                                </div>
+                            </form>
+                            <div className="mx-auto my-4 flex w-full items-center justify-evenly
+                            before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
+                            after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                            o
+                            </div>
+                            <p className="text-center text-sm text-black mt-2">
+                            ¿No tienes cuenta?
+                                <Link className='text-teal-600' href='/register'> Regístrate</Link>
+                            </p>
+                        </Form>
                     </div>
-                    <p className="text-center text-sm text-black mt-2">
-                    ¿No tienes cuenta?
-                        <Link className='text-teal-600' href='/register'> Regístrate</Link>
-                    </p>
-                </Form>
-            </div>
+                </div>
             </Auth>
         </RootLayout>
       )
--- src/containers/Views/public/SignUp/index.tsx
+++ src/containers/Views/public/SignUp/index.tsx
@@ -30,100 +30,102 @@ export function SignUpView(){
     return (
         <RootLayout>
           <Auth imageSrc='/images/business_and_work_planning.png'>
-            <div className="max-w-md mx-auto bg-white p-8 rounded-3xl shadow-md w-full">
-            <p className="text-center mb-1">Crea tu cuenta</p>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="mail@example.com" 
-                        type = 'email'
-                        {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="first_name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Nombre</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter your name" 
-                        type = 'name'
-                        {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="last_name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Apellidos</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter your last name" 
-                        type = 'last_name'
-                        {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Contraseña</FormLabel>
-                      <FormControl>
-                        <Input 
-                        type = 'password'
-                        placeholder="Enter your password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="public_name"
-                  render={({ field }) => (
-                    <FormItem className="mb-4">
-                      <FormLabel>Nombre de usuario</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter your username" 
-                        type = 'public_name'
-                        {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="text-center">
-                  <Button type="submit">REGISTRARSE</Button>
-                </div>
-              </form>
-              <div className="mx-auto my-4 flex w-full items-center justify-evenly
-              before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
-              after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
-                o
+            <div className="mx-auto bg-gray-200 p-8 rounded-3xl shadow-md w-2/5">
+              <p className="text-center text-2xl">Crea tu cuenta</p>
+              <div className="mx-auto bg-white p-8 rounded-3xl shadow-md w-full">
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="mail@example.com" 
+                            type = 'email'
+                            {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="first_name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nombre</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Enter your name" 
+                            type = 'name'
+                            {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="last_name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Apellidos</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Enter your last name" 
+                            type = 'last_name'
+                            {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Contraseña</FormLabel>
+                          <FormControl>
+                            <Input 
+                            type = 'password'
+                            placeholder="Enter your password" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="public_name"
+                      render={({ field }) => (
+                        <FormItem className="mb-4">
+                          <FormLabel>Nombre de usuario</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Enter your username" 
+                            type = 'public_name'
+                            {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <div className="text-center">
+                      <Button type="submit">REGISTRARSE</Button>
+                    </div>
+                  </form>
+                  <div className="mx-auto my-4 flex w-full items-center justify-evenly
+                  before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
+                  after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                    o
+                  </div>
+                  <p className="text-center text-sm text-black mt-2">
+                    ¿Ya tienes cuenta?
+                    <Link className='text-teal-600' href='/login'> Inicia sesión</Link>
+                  </p>
+                </Form>
               </div>
-              <p className="text-center text-sm text-black mt-2">
-                ¿Ya tienes cuenta?
-                <Link className='text-teal-600' href='/login'> Inicia sesión</Link>
-              </p>
-            </Form>
-          </div>
+            </div>
           </Auth>
         </RootLayout>
       )
