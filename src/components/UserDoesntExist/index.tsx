67238cf7ece402eee4b9bf7cb8684577d40d9135
Carmen <carmengalvan@z1.digital>
2024-04-05 12:26:46 +0200

    feat: error page in case the user does not exist or does not have resources

--- src/components/UserDoesntExist/index.tsx
+++ src/components/UserDoesntExist/index.tsx
@@ -4,10 +4,23 @@ interface UserDoesntExistProps {
 
 export const UserDoesntExist = ({ publicName }: UserDoesntExistProps) => {
 	return (
-		<div className="bg-gray-100 p-8 rounded-lg shadow-md">
-			<p className="text-lg text-gray-800 text-center">
-				El usuario {publicName} no existe o no tiene recursos disponibles.
-			</p>
+		<div className="bg-green-200 min-h-screen flex justify-center items-center relative">
+			<img
+				src="/images/error_image.png"
+				alt="Error"
+				className="absolute left-0 top-0 w-1/2 h-full"
+			/>
+
+			<div className="p-8 absolute right-0 mr-16 flex flex-col justify-center items-center">
+				<h1 className="text-3xl font-bold mb-4">
+					Ups! Parece que hubo un problema.
+				</h1>
+				<p className="text-lg text-gray-800">
+					El usuario {publicName} no existe o no tiene recursos disponibles.
+				</p>
+				<p className="text-lg text-gray-800">¡Inténtalo de nuevo!</p>
+				<p className="text-sm text-gray-600">Error 404</p>
+			</div>
 		</div>
 	);
 };
